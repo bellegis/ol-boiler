e60623435ac91dba230138473adaaf9010491812
Isabelle Rose Lipton <belle_lipton@harvard.edu>
2022-03-08 10:26:37 -0500

    add hmc wms example

--- src/js/index.js
+++ src/js/index.js
@@ -3,20 +3,37 @@ import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import TileWMS from 'ol/source/TileWMS';
+
+
+//define the layers, including the basemap and a HMC HGL map
+const layers = [
+  new TileLayer({
+    source: new OSM()
+  }),
+  new TileLayer({
+    source: new TileWMS({
+      url: 'https://geodata-proxy.lib.harvard.edu/geoserver/proxy/requestfile/wms',
+      attributions:
+        'Harvard Map Collection',
+      params: {
+        'LAYERS': 'mapimages_public:G5834_P3_1889_V8',
+      },
+    }),
+  }),
+];
+
 
 //initialize the map
 const map = new Map({
-  layers: [
-    new TileLayer({
-      source: new OSM()
-    })
-  ],
+  layers: layers,
   target: 'map',
   view: new View({
-    center: [-8111697.401311302,2450572.315615977],
-    zoom: 5
+    center: [260221.8433442565,6251346.86217431],
+    zoom: 16
   })
 });
+
 
 //function to get the current extent and zoom level 
 function getBounds() {
